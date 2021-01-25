9f9768b640429e3867c971965d0f946de2153c9f
Aaron Hales <aaron.hales@tooeleschools.org>
2021-01-25 10:10:34 -0700

    added values to spinner, strings to resource, and started developing the button method

--- BeerAdvisor/app/src/main/java/com/example/myapplication/MainActivity.java
+++ BeerAdvisor/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClick_findDrink(View view) {
+        TextView brands = (TextView) findViewById(R.id.brands);
+        brands.setText("you clicked the button");
+
+    }
+
+
 }
